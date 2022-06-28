cd93a6a16b591ba8e1f1162663b5c00fd6d5764f
Kadirgu <Kadirgu@users.noreply.github.com>
2022-06-28 15:48:16 +0200

    update after call with mentor

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const Users = Models.User;
 dotenv.config();
 
 
-mongoose.connect(process.env.CONNECTION_URI, { useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://Kadirgu:Kadirgu123@myflixdb.kw91e.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true });
 
 
 app.use(bodyParser.json());
@@ -31,16 +31,9 @@ require('./passport');
 const cors = require('cors');
 app.use(cors());
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors(
+
+));
 
 /* rest of code goes here*/
 
@@ -303,6 +296,6 @@ app.use((err, req, res, next) => {
 
 //app.listen(PORT, ()=>console.log("App is running"));
 
-app.listen(process.env.PORT, '0.0.0.0', () => {
-  console.info('Listening on Port ' + process.env.PORT);
+app.listen(2222, '0.0.0.0', () => {
+  console.info('Listening on Port ' + "2222");
 });
