6a31ecf6a29ac7f2dd9474ae7b05370b39d49a3c
Kadirgu <Kadirgu@users.noreply.github.com>
2022-07-04 13:02:06 +0200

    update app use CORS

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //importing CORS
 const cors = require("cors");
 app.use(cors());
+app.use((req,res,next)=>{
+res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin',
+           'Origin, X-Requested-with, Content_Type,Accept, Authorization',
+           'localhost:2222');
+if(req.method === 'OPTIONS') {
+res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+return res.status(200).json({});
+}
+next();
+});
 
 const Movies = Models.Movie;
 const Users = Models.User;
