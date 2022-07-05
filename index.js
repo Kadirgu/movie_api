74f166657919f3330d4a57a6af72d36bced11205
Kadirgu <Kadirgu@users.noreply.github.com>
2022-07-05 11:51:08 +0200

    update

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Credentials", true);
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
